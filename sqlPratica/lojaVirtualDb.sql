f08f18cd3e27fe9596d181dac0c7b1bfbe83763b
Alvaro Lima <alvarolima1103@hotmail.com>
2020-08-12 09:48:28 -0400

    Finalizado as consultas na tabela

--- sqlPratica/lojaVirtualDb.sql
+++ sqlPratica/lojaVirtualDb.sql
@@ -75,3 +75,97 @@ insert into tb_pedidos_produtos(id_pedido, id_produto,) values (2, 3);
 insert into tb_clientes(nome, idade) values ('Jamilton', 30);
 insert into tb_pedidos(id_cliente) values (2);
 insert into tb_pedidos_produtos(id_pedido, id_produto) values (3, 1);
+
+/*JOIN - Juncao a esquerda (LEFT JOIN)*/
+select 
+	* 
+from 
+	tb_clientes left join tb_pedidos on (tb_clientes.id_cliente = tb_pedidos.id_cliente);
+
+select 
+	* 
+from 
+	tb_produtos left join tb_imagens on (tb_produtos.id_produto = tb_imagens.id_produto);
+
+/*JOIN - Juncao a direita (RIGHT JOIN)*/
+select 
+	*
+from 
+	tb_clientes right join tb_pedidos on (tb_clientes.id_cliente = tb_pedidos.id_cliente);
+
+/*JOIN - Juncao a direita (RIGHT JOIN)*/
+insert into tb_produtos(produto, valor) values ('HD Externo Portatil Seagate Expansion 1TB USB 3.0',274.90);
+
+select 
+	*
+from 
+	tb_pedidos left join tb_pedidos_produtos on (tb_pedidos.id_pedido = tb_pedidos_produtos.id_pedido)
+	left join tb_produtos on (tb_pedidos_produtos.id_produto = tb_produtos.id_produto);
+
+select 
+	*
+from 
+	tb_pedidos right join tb_pedidos_produtos on (tb_pedidos.id_pedido = tb_pedidos_produtos.id_pedido)
+	left join tb_produtos on (tb_pedidos_produtos.id_produto = tb_produtos.id_produto);
+
+select 
+	*
+from 
+	tb_pedidos right join tb_pedidos_produtos on (tb_pedidos.id_pedido = tb_pedidos_produtos.id_pedido)
+	right join tb_produtos on (tb_pedidos_produtos.id_produto = tb_produtos.id_produto);
+
+select 
+	*
+from 
+	tb_pedidos inner join tb_pedidos_produtos on (tb_pedidos.id_pedido = tb_pedidos_produtos.id_pedido);
+
+select 
+	*
+from 
+	tb_pedidos inner join tb_pedidos_produtos on (tb_pedidos.id_pedido = tb_pedidos_produtos.id_pedido)
+	inner join tb_produtos on (tb_pedidos_produtos.id_produto = tb_produtos.id_produto);
+
+
+/* Alias - Apelidando tabelas */
+select 
+	*
+from 
+	tb_produtos as p left join tb_descricoes_tecnicas as dt on (p.id_produto = dt.id_produto);
+
+select 
+	tb_produtos.id_produtos,
+	tb_produtos.produto,
+	tb_produtos.valor,
+	tb_descricoes_tecnicas.descricao_tecnica
+from 
+	tb_produtos as p left join tb_descricoes_tecnicas as dt on (p.id_produto = dt.id_produto);
+
+select 
+	p.id_produto,
+	p.produto,
+	p.valor,
+	dt.descricao_tecnica
+from 
+	tb_produtos as p left join tb_descricoes_tecnicas as dt on (p.id_produto = dt.id_produto);
+
+select 
+	p.id_produto,
+	p.produto,
+	p.valor,
+	dt.descricao_tecnica
+from 
+	tb_produtos as p left join tb_descricoes_tecnicas as dt on (p.id_produto = dt.id_produto)
+where
+	p.valor >= 500;
+
+select 
+	p.id_produto,
+	p.produto,
+	p.valor,
+	dt.descricao_tecnica
+from 
+	tb_produtos as p left join tb_descricoes_tecnicas as dt on (p.id_produto = dt.id_produto)
+where
+	p.valor >= 500
+order by
+	p.valor asc;
