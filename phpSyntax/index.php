f379ef65e193a8c8b293c69cf39eee3892a9df4d
Alvaro Lima <alvarolima1103@outlook.com>
2021-02-19 22:49:14 -0400

    Praticando a sitaxe

--- phpSyntax/index.php
+++ phpSyntax/index.php
@@ -1,17 +1,49 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 	<head>
 		<meta charset="UTF-8">
 		<title>Curso PHP</title>
 	</head>
 	<body>
 		<?php
-			echo 'Utilizando a tag php';
-		?>
-		<br/>
-		<?= "Utilizando a tag impressao" ?>
-		<br/>
-		<? echo 'Utilizanod  a tag curta' ?>
 
+
+			$usuario_possui_cartao_loja = true;
+			$valor_compra = 225;
+
+			$valor_frete = 50;
+
+			$recebeu_deconto_frete = false;
+
+			if($usuario_possui_cartao_loja == true && $valor_compra >= 100) {
+				$valor_frete = 0;
+				$recebeu_deconto_frete = true;
+			}
+		?>
+
+		<h1>Detalhes do pedido</h1>
+		<p> Possui cartao da loja?
+			<?php
+				if($usuario_possui_cartao_loja == true) {
+					echo 'Sim';
+				} else {
+					echo 'Nao';
+				}
+			?>
+		</p>
+		<p>Valor da compra: <?= $valor_compra?> </p>
+
+
+		<p> Recebeu descomto no frete?
+			<?php
+				if($usuario_possui_cartao_loja == true) {
+					echo 'Sim';
+				} else {
+					echo 'Nao';
+				}
+			?>
+		</p>
+
+		<p>Valor da compra: <?= $valor_frete?> </p>
 	</body>
 </html>
