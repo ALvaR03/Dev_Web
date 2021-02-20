5107081261a67f51fc0f359d7596e9f3acf9df2d
Alvaro Lima <alvarolima1103@outlook.com>
2021-02-19 23:06:31 -0400

    Operadores ternarios

---
+++ phpSyntax/ternario_Operador.php
@@ -0,0 +1,14 @@
+<?php
+    // <condicao> ? true : false  
+
+    $test = $usuario_possui_cartao_loja ? 'Sim' : 'Nao';
+
+    echo $teste;
+
+    if ($teste) {
+        echo 'sim';
+    } else {
+        echo 'nao';
+    }
+
+?>
